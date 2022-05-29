d6154242d584c67dec9d4083aa54f1dc5042933d
Felipe Borges <felipe.borges@olist.com>
2022-05-29 18:36:31 -0300

    feat: add images to assets folder

---
+++ src/assets/index.ts
@@ -0,0 +1,4 @@
+export { default as LogoSVG } from './images/logo.svg';
+export { default as WalletSVG } from './images/wallet.svg';
+export { default as EtherSVG } from './images/ether.svg';
+export { default as DeleteSVG } from './images/delete.svg';
