42cd1e446704f790cbd0abd02d390322dc1cba22
Felipe Borges <felipe.borges@olist.com>
2022-06-01 21:56:45 -0300

    test: add test to etherprice component

---
+++ src/components/EtherPrice/index.test.tsx
@@ -0,0 +1,19 @@
+import { renderWithEverything, screen } from '../../helpers/rtl';
+
+import EtherPrice from '.';
+
+describe('EtherPrice', () => {
+  it('should show the component correctly with value 10', () => {
+    renderWithEverything(<EtherPrice value={10} />);
+
+    expect(screen.queryByText('10.00')).toBeInTheDocument();
+    expect(screen.queryByText('10.00')).toHaveStyle('font-size: 16px;');
+    expect(screen.queryByAltText('Logo da moeda Ether')).toBeInTheDocument();
+  });
+
+  it('should render de value with font-size equal to 24px', () => {
+    renderWithEverything(<EtherPrice value={10} fontSize="24px" />);
+
+    expect(screen.queryByText('10.00')).toHaveStyle('font-size: 24px;');
+  });
+});
